a980f62bc74db5a3b9a83fe43dd7fd1dace09e80
judeakinwale <judeakinwale@gmail.com>
2022-04-21 11:53:05 +0100

    Refactor auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
-const { verify } = require("jsonwebtoken");
-// const jwt = require("jsonwebtoken")
-const Staff = require("../models/Staff");
+const Staff = require("../models/Staff")
+const { verify } = require("jsonwebtoken")
 const {ErrorResponseJSON} = require("../utils/errorResponse")
 
   
@@ -17,37 +16,23 @@ exports.verifyToken = async (req, res, next) => {
     }
 
     if (!token) {
-      return next(new ErrorResponseJSON(res, "You need to be logged in to perform this action.", 401))
+      return new ErrorResponseJSON(res, "You need to be logged in to perform this action.", 401)
     }
-
-    // verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    //   if (err) {
-    //     return res
-    //       .status(403)
-    //       .json({ message: "You are not authorized. Token is invalid." });
-    //   }
-    //   const { staff } = decoded;
-    //   req.user = staff._id; //tokenize the user's id
-    //   next();      
-    // });
 
     try {
-      // Verify token
-      // const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const decoded = verify(token, process.env.JWT_SECRET);
+      const decoded = verify(token, process.env.JWT_SECRET) // Verify token
       // staff is returned when verifying the token
       req.staff = decoded.staff
-      // the same as req.staff
-      req.user = await Staff.findById(decoded.staff._id);
+      req.user = await Staff.findById(decoded.staff._id) // the same as req.staff
   
-      next();
+      next()
     } catch (err) {
-      return next(new ErrorResponseJSON(res, " Token is invalid.", 401));
+      return new ErrorResponseJSON(res, " Token is invalid.", 401)
     }
   } catch (err) {
-    return next(new ErrorResponseJSON(res, "Not authorized to access this route", 401))
+    return new ErrorResponseJSON(res, "Not authorized to access this route", 401)
   }
-};
+}
 
 
 // Grant access to specific roles
@@ -56,8 +41,8 @@ exports.authorize = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorResponseJSON(res, `User role ${req.user.role} is not authorized to access this route`, 403)
-      );
+      )
     }
-    next();
-  };
+    next()
+  }
 }
