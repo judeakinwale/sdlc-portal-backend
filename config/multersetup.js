813d1215c5f43a06a059058458b7a217d881214a
judeakinwale <judeakinwale@gmail.com>
2022-04-22 08:09:12 +0100

    Minor changes

--- config/multersetup.js
+++ config/multersetup.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, '/public/uploads')
+    callback(null, './public/uploads')
   },
   filename: (req, file, callback) => {
     callback(null, Date.now() + file.originalname);
